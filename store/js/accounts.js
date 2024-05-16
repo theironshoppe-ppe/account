72d343127bb233f62e2a0de7db7cc9afe928ed2e
theironshoppe-ppe <154846458+theironshoppe-ppe@users.noreply.github.com>
2024-05-16 09:58:07 -0700

    Create accounts.js

---
+++ store/js/accounts.js
@@ -0,0 +1,6 @@
+var account;
+function getaccount() {
+  if (('script').hasAttribute('src') {
+    var account = ('script').getAttribute('src');
+    if (account == 'account.js') {
+      return account;
